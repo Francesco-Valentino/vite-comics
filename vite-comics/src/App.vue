738b573f9c60a93fc3740b511e2d3d10ac49cfcf
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-15 13:47:15 +0200

    initializing components

--- vite-comics/src/App.vue
+++ vite-comics/src/App.vue
@@ -1,19 +1,25 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default{
+  data() {
+    return {
+      
+    }
+  },
+
+  components: {
+    HelloWorld,
+}
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <HelloWorld/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
+<style>
 @import url(styles/general.scss);
 </style>
