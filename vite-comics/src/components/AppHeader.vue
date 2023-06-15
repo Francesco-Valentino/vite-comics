df72e15c261dbc6184725767f1a51b957f33aaa7
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-06-15 16:38:26 +0200

    set header basic style

--- vite-comics/src/components/AppHeader.vue
+++ vite-comics/src/components/AppHeader.vue
@@ -1,8 +1,72 @@
 <template>
     <header>
-        <div class="container">
-            
-        </div>
+        <nav class="container">
+            <div id="brandLogo">
+                <img src="../assets/img/dc-logo.png" alt="DC Comics brand logo">
+            </div>
+
+            <div id="navigation">
+                <div class="element">
+                    <p>
+                        Characters
+                    </p>
+                </div>
+
+                <div class="element">
+                    <p>
+                        Comics
+                    </p>
+                </div>
+
+                <div class="element">
+                    <p>
+                        Movies
+                    </p>
+                </div>
+
+                <div class="element">
+                    <p>
+                        TV
+                    </p>
+                </div>
+
+                <div class="element">
+                    <p>
+                    Games
+                    </p>
+                </div>
+
+                <div class="element">
+                    <p>
+                        Collectibles
+                    </p>
+                </div>
+
+                <div class="element">
+                    <p>
+                        Videos
+                    </p>
+                </div>
+
+                <div class="element">
+                    <p>
+                        Fans
+                    </p>
+                </div>
+
+                <div class="element">
+                    <p>
+                        News
+                    </p>
+                </div>
+
+                <div class="element">
+                    <p>
+                        Shop
+                    </p>
+                </div>
+            </div>
+        </nav>
     </header>
 </template>
 
@@ -12,6 +76,28 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+    @use '../styles/partials/mixins' as *;
+    @use '../styles/partials/variables' as *;
 
+    nav.container{
+        display: flex;
+        justify-content: space-between;
+
+        div#brandLogo img{
+            width: 70px;
+        }
+
+        div#navigation{
+            display: flex;
+            justify-content: space-between;
+            flex-wrap: nowrap;
+            text-transform: uppercase;
+
+            p{
+                font-size: .6rem;
+                padding: 0 .7rem;
+            }
+        }
+    }
 </style>
